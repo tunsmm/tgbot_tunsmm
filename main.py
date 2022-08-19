a1c66d353210a044217b57d9961f315a688aae51
tunsmm <dragon553180@gmail.com>
2022-08-19 09:31:05 +0300

    Simple program to send a message

---
+++ main.py
@@ -0,0 +1,16 @@
+import telegram
+import os
+
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+def send_message(message):
+    return bot.send_message(chat_id=CHAT_ID, text=message)
+
+send_message('Hello world!')
